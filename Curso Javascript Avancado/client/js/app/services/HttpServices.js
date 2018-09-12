ffa56d6c5cc6dd9b827172f45af304d94c5bdb30
José Luiz <jose.iscosistemas@outlook.com>
2018-09-12 17:58:40 -0300

    Finalização do Segundo Módulo

--- Curso Javascript Avancado/client/js/app/services/HttpServices.js
+++ Curso Javascript Avancado/client/js/app/services/HttpServices.js
@@ -1,5 +1,5 @@
 class   HttpServices{
-    
+//get
     get(url){
         return new Promise((resolve, reject) =>{
             let xhr = new XMLHttpRequest();
@@ -18,5 +18,31 @@ class   HttpServices{
             };
             xhr.send();
         });
+    }
+
+    post(url, dado) {
+
+
+        return new Promise((resolve, reject) => {
+
+            let xhr = new XMLHttpRequest();
+            xhr.open("POST", url, true);
+            xhr.setRequestHeader("Content-type", "application/json");
+            xhr.onreadystatechange = () => {
+
+                if (xhr.readyState == 4) {
+
+                    if (xhr.status == 200) {
+
+                        resolve(JSON.parse(xhr.responseText));
+                    } else {
+
+                        reject(xhr.responseText);
+                    }
+                }
+            };
+            xhr.send(JSON.stringify(dado)); // usando JSON.stringify para converter objeto em uma string no formato JSON.
+        });
+
     }
 }
